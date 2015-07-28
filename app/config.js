d6242ae0a8b8d55b64633df1d95bd58f77a8cc66
MKS-students <mks.students@hackreactor.com>
2015-07-27 17:30:25 -0700

    Implements sessions and signup

--- app/config.js
+++ app/config.js
@@ -29,6 +29,7 @@ db.knex.schema.hasTable('urls').then(function(exists) {
   }
 });
 
+
 db.knex.schema.hasTable('clicks').then(function(exists) {
   if (!exists) {
     db.knex.schema.createTable('clicks', function (click) {
@@ -44,6 +45,19 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
 /************************************************************/
 // Add additional schema definitions below
 /************************************************************/
+
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.increments('id').primary();
+      user.string('user_name',25);
+      user.string('password',255);
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
 
 
 module.exports = db;
--- shortly.js
+++ shortly.js
@@ -2,6 +2,7 @@ var express = require('express');
 var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 
 var db = require('./app/config');
@@ -21,26 +22,26 @@ app.use(bodyParser.json());
 // Parse forms (signup/login)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}));
 
-
-app.get('/', 
+app.get('/', util.checkAuthentication,
 function(req, res) {
   res.render('index');
 });
 
-app.get('/create', 
+app.get('/create', util.checkAuthentication,
 function(req, res) {
   res.render('index');
 });
 
-app.get('/links', 
+app.get('/links', util.checkAuthentication,
 function(req, res) {
   Links.reset().fetch().then(function(links) {
     res.send(200, links.models);
   });
 });
 
-app.post('/links', 
+app.post('/links', util.checkAuthentication,
 function(req, res) {
   var uri = req.body.url;
 
@@ -77,7 +78,52 @@ function(req, res) {
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
+app.get('/login', function(req, res) {
+  res.render('login');
+});
 
+app.post('/login', function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  console.log(username, password);
+  // receive username and password on req.body
+  // check if username exists in db
+    // if yes
+      // hash user-input passwords
+      //compare w/ password in db
+        // if passwords match
+          // start a session
+          // redirect to homepage
+    // if no
+      // send back username / password error message
+});
+
+app.get('/signup', function(req, res) {
+  res.render('signup');
+});
+
+app.post('/signup', function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  console.log(username, password);
+  // receive username and password on req.body
+  // check if username exists in db
+  util.checkUserInDB(username)
+    .then(function(isFound) {
+    // if yes
+      if ( isFound ) {
+      // send username already exists error
+        return res.send("Username already exists.");
+      }
+    // if no
+      // add username and hashed password to db
+      util.createNewUser(username, password)
+        .then(function(model) {
+          // call util.createSession
+          util.createSession(req, res);
+        });
+    });
+});
 
 
 /************************************************************/
